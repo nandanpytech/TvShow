67635360b980e2ee9e885df4e037cd2937a62aab
nandanpytech <bilaginandan333@gmail.com>
2023-05-10 22:50:31 +0530

    Navbar updated

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Link } from 'react-router-dom';
-import CircleRoundedIcon from '@mui/icons-material/CircleRounded';
+import { Link } from "react-router-dom";
+import CircleRoundedIcon from "@mui/icons-material/CircleRounded";
 export default function Home() {
   const [showdata, setShowData] = useState([]);
   useEffect(() => {
@@ -18,41 +18,49 @@ export default function Home() {
   return (
     <>
       <div className="container">
-        <h2 className="mt-4 mb-4">TV Shows</h2>
+        <div className="header d-flex flex-lg-row flex-column justify-content-between" >
+          <h2 className="mt-4 mb-4">TV Shows</h2>
+          <form className="d-flex col-sm-6 align-self-center mb-4 mb-md-0" style={{height:"fit-content"}} role="search">
+            <input
+              className="form-control me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+            />
+            <button className="btn btn-outline-success" type="submit">
+              Search
+            </button>
+          </form>
+        </div>
+
         <div className="row justify-content-lg-between justify-content-md-around justify-content-center">
-        {showdata.map((showItem, index) => {
-          const show = showItem.show;
-          const {image, name, language, genres } = show;
-          return (
+          {showdata.map((showItem, index) => {
+            const show = showItem.show;
+            const { image, name, language, genres } = show;
+            return (
               <div key={index} class="card mb-4" style={{ width: "16rem" }}>
-                <img src={image?.medium} class="card-img-top" alt="No Image Found" />
+                <img
+                  src={image?.medium}
+                  class="card-img-top"
+                  alt="No Image Found"
+                />
                 <div class="card-body">
                   <h5 class="card-title">{name}</h5>
                   <div className="details d-flex justify-content-between">
-                  <p class="card-text">
-                    {language}
-                   </p>
-                    <CircleRoundedIcon sx={{width:"7px"}}/>
-                  <p class="card-text">
-                    {genres && genres[0]}
-                   </p>
-                   {
-                       genres[1] &&
-                    <CircleRoundedIcon sx={{width:"7px"}}/>
-                   }
-                  <p class="card-text">
-                    {genres && genres[1]}
-                   </p>
+                    <p class="card-text">{language}</p>
+                    <CircleRoundedIcon sx={{ width: "7px" }} />
+                    <p class="card-text">{genres && genres[0]}</p>
+                    {genres[1] && <CircleRoundedIcon sx={{ width: "7px" }} />}
+                    <p class="card-text">{genres && genres[1]}</p>
                   </div>
-        
+
                   <Link to={`/show/${index}`} class="btn btn-outline-primary ">
                     More
                   </Link>
                 </div>
               </div>
-           
-          );
-        })}
+            );
+          })}
         </div>
       </div>
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,23 +4,41 @@ import { Link } from "react-router-dom";
 export default function NavBar() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg  navbar-dark bg-dark">
-        <div className="container-fluid">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
           <Link className="navbar-brand order-sm-0" to="/">
             MovieTime
           </Link>
-
-          <form className="d-flex col-sm-6 " role="search">
-              <input
-                className="form-control me-2"
-                type="search"
-                placeholder="Search"
-                aria-label="Search"
-              />
-              <button className="btn btn-outline-success" type="submit">
-                Search
-              </button>
-            </form>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav  mb-2 mb-lg-0" style={{marginLeft:"auto",gap:"2rem"}}>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  About
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     </>
