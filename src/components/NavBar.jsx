5a02179a50dda1c77cbc2ec36bbfe34ee131abc1
nandanpytech <bilaginandan333@gmail.com>
2023-05-10 16:48:44 +0530

    updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -20,39 +20,6 @@ export default function NavBar() {
                 Search
               </button>
             </form>
-
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button>
-
-          <div className="collapse navbar-collapse" id="navbarSupportedContent" style={{maxWidth:"max-content"}}>
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0 ml-md-5" style={{gap:"2rem"}}>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Home
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  About
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
-                  Contact Us
-                </a>
-              </li>
-             
-            </ul>
-          </div>
         </div>
       </nav>
     </>
